613e0a486dde9287d52db081e07cfbf7f3c36c66
Haocen2004 <haocen.minecraft@qq.com>
2021-04-03 14:51:43 +0800

    fix update dialog
    Signed-off-by: Haocen2004 <haocen.minecraft@qq.com>

--- app/src/main/java/com/github/haocen2004/login_simulation/Activity/MainActivity.java
+++ app/src/main/java/com/github/haocen2004/login_simulation/Activity/MainActivity.java
@@ -1,7 +1,12 @@
 package com.github.haocen2004.login_simulation.Activity;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -9,24 +14,47 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.github.haocen2004.login_simulation.Database.SponsorRepo;
 import com.github.haocen2004.login_simulation.R;
 import com.github.haocen2004.login_simulation.databinding.ActivityMainBinding;
+import com.github.haocen2004.login_simulation.util.Logger;
+import com.github.haocen2004.login_simulation.util.Network;
+
+import org.json.JSONObject;
+
+import java.util.concurrent.Executors;
+
+import cn.leancloud.AVLogger;
+import cn.leancloud.AVOSCloud;
+import cn.leancloud.AVUser;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 import static com.github.haocen2004.login_simulation.BuildConfig.DEBUG;
+import static com.github.haocen2004.login_simulation.BuildConfig.VERSION_CODE;
 import static com.github.haocen2004.login_simulation.BuildConfig.VERSION_NAME;
+import static com.github.haocen2004.login_simulation.util.Constant.BH_VER;
+import static com.github.haocen2004.login_simulation.util.Constant.CHECK_VER;
+import static com.github.haocen2004.login_simulation.util.Constant.HAS_ACCOUNT;
+import static com.github.haocen2004.login_simulation.util.Constant.MDK_VERSION;
+import static com.github.haocen2004.login_simulation.util.Constant.SP_URL;
+import static com.github.haocen2004.login_simulation.util.Tools.openUrl;
 
 public class MainActivity extends AppCompatActivity {
     private NavController navController;
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-
+    private SharedPreferences app_pref;
+    private Logger Log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        app_pref = getDefaultSharedPreferences(this);
+        Log = Logger.getLogger(this);
 //        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(binding.mainInclude.toolbar);
         navController = Navigation.findNavController(this, R.id.hostFragment);
@@ -56,7 +84,116 @@ public class MainActivity extends AppCompatActivity {
         if (getDefaultSharedPreferences(this).getBoolean("showBetaInfo", DEBUG)) {
             showBetaInfoDialog();
         }
+
+        if (CHECK_VER) {
+            new Thread(update_rb).start();
+        } else {
+            BH_VER = app_pref.getString("bh_ver", BH_VER);
+            MDK_VERSION = app_pref.getString("mdk_ver", MDK_VERSION);
+            AVOSCloud.initialize(this, "VMh6lRyykuNDyhXxoi996cGI-gzGzoHsz", "RWvHCY9qXzX1BH4L72J9RI1I", SP_URL);
+            if (DEBUG) {
+                AVOSCloud.setLogLevel(AVLogger.Level.DEBUG);
+            }
+        }
     }
+
+    @SuppressLint("HandlerLeak")
+    Handler update_check_hd = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            Bundle data = msg.getData();
+            String feedback = data.getString("value");
+            feedback = feedback.substring(1, feedback.length() - 1).replaceAll("\\\\", "");
+            Logger.i("Update", "handleMessage: " + feedback);
+            try {
+                JSONObject json = new JSONObject(feedback);
+                app_pref.edit().putString("bh_ver", json.getString("bh_ver"))
+                        .putString("mdk_ver", json.getString("mdk_ver"))
+                        .putString("sp_url", json.getString("sp_url")).apply();
+                Logger.d("Update", "cloud ver:" + json.getInt("ver"));
+                Logger.d("Update", "local ver:" + VERSION_CODE);
+                Logger.d("Update", "pack name contains dev:" + getPackageName().contains("dev"));
+                if (!getPackageName().contains("dev") && (VERSION_CODE < json.getInt("ver"))) {
+                    Logger.i("Update", "Start Update window");
+                    showUpdateDialog(
+                            json.getString("ver_name"),
+                            json.getString("update_url"),
+                            json.getString("logs").replaceAll("&n", "\n")
+                    );
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Logger.d("Update", "Check Update Failed");
+
+                //app_pref.edit().putString("bh_ver", BH_VER).apply();
+            }
+            BH_VER = app_pref.getString("bh_ver", BH_VER);
+            MDK_VERSION = app_pref.getString("mdk_ver", MDK_VERSION);
+            SP_URL = app_pref.getString("sp_url", SP_URL);
+            AVOSCloud.initialize(getApplicationContext(), "VMh6lRyykuNDyhXxoi996cGI-gzGzoHsz", "RWvHCY9qXzX1BH4L72J9RI1I", SP_URL);
+            if (DEBUG) {
+                AVOSCloud.setLogLevel(AVLogger.Level.DEBUG);
+            }
+
+            Executors.newSingleThreadExecutor().execute(() -> new SponsorRepo(getApplicationContext()).refreshSponsors());
+            if (app_pref.getBoolean("has_account", false)) {
+                String TAG = "login check";
+
+                Logger.d(TAG, "Start.");
+                Executors.newSingleThreadExecutor().execute(() -> {
+                    AVUser.becomeWithSessionTokenInBackground(app_pref.getString("account_token", "")).subscribe(new Observer<AVUser>() {
+                        public void onSubscribe(Disposable disposable) {
+                        }
+
+                        public void onNext(AVUser user) {
+                            AVUser.changeCurrentUser(user, true);
+                            HAS_ACCOUNT = true;
+                            app_pref.edit().putString("custom_username", user.getString("custom_username")).apply();
+                            Logger.d(TAG, "Succeed.");
+
+                        }
+
+                        public void onError(Throwable throwable) {
+                            AVUser.changeCurrentUser(null, true);
+                            app_pref.edit().putBoolean("has_account", false)
+                                    .putString("custom_username", "崩坏3扫码器用户").apply();
+                            throwable.printStackTrace();
+                            Logger.d(TAG, "Failed.");
+                            Log.makeToast("赞助者身份验证已过期...");
+                        }
+
+                        public void onComplete() {
+                        }
+                    });
+                });
+
+            }
+        }
+    };
+
+
+    private void showUpdateDialog(String ver, String url, String logs) {
+        final AlertDialog.Builder normalDialog = new AlertDialog.Builder(this);
+        normalDialog.setTitle("获取到新版本: " + ver);
+        normalDialog.setMessage("更新日志：\n" + logs);
+        normalDialog.setPositiveButton("打开更新链接",
+                (dialog, which) -> {
+                    openUrl(url, this);
+                    dialog.dismiss();
+                });
+        normalDialog.setCancelable(false);
+        normalDialog.show();
+    }
+
+    Runnable update_rb = () -> {
+        String feedback = Network.sendPost("https://service-beurmroh-1256541670.sh.apigw.tencentcs.com/version", "");
+        Message msg = new Message();
+        Bundle data = new Bundle();
+        data.putString("value", feedback);
+        msg.setData(data);
+        update_check_hd.sendMessage(msg);
+    };
 
     @Override
     public boolean onSupportNavigateUp() {
--- app/src/main/java/com/github/haocen2004/login_simulation/MainApplication.java
+++ app/src/main/java/com/github/haocen2004/login_simulation/MainApplication.java
@@ -1,40 +1,18 @@
 package com.github.haocen2004.login_simulation;
 
-import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDelegate;
 
-import com.github.haocen2004.login_simulation.Database.SponsorRepo;
 import com.github.haocen2004.login_simulation.util.Logger;
-import com.github.haocen2004.login_simulation.util.Network;
 import com.tencent.bugly.crashreport.CrashReport;
-
-import org.json.JSONObject;
-
-import java.util.concurrent.Executors;
-
-import cn.leancloud.AVLogger;
-import cn.leancloud.AVOSCloud;
-import cn.leancloud.AVUser;
-import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
 
 import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 import static com.github.haocen2004.login_simulation.BuildConfig.DEBUG;
 import static com.github.haocen2004.login_simulation.BuildConfig.VERSION_CODE;
-import static com.github.haocen2004.login_simulation.util.Constant.BH_VER;
 import static com.github.haocen2004.login_simulation.util.Constant.CHECK_VER;
-import static com.github.haocen2004.login_simulation.util.Constant.HAS_ACCOUNT;
 import static com.github.haocen2004.login_simulation.util.Constant.MDK_VERSION;
-import static com.github.haocen2004.login_simulation.util.Constant.SP_URL;
-import static com.github.haocen2004.login_simulation.util.Tools.openUrl;
 
 public class MainApplication extends Application {
     private SharedPreferences app_pref;
@@ -100,49 +78,6 @@ public class MainApplication extends Application {
         }
         CHECK_VER = app_pref.getBoolean("check_update", true);
 
-        if (CHECK_VER) {
-            new Thread(update_rb).start();
-        } else {
-            BH_VER = app_pref.getString("bh_ver", BH_VER);
-            MDK_VERSION = app_pref.getString("mdk_ver", MDK_VERSION);
-            AVOSCloud.initialize(this, "VMh6lRyykuNDyhXxoi996cGI-gzGzoHsz", "RWvHCY9qXzX1BH4L72J9RI1I", SP_URL);
-            if (DEBUG) {
-                AVOSCloud.setLogLevel(AVLogger.Level.DEBUG);
-            }
-        }
-
-        if (app_pref.getBoolean("has_account", false)) {
-            String TAG = "login check";
-
-            Logger.d(TAG, "Start.");
-            Executors.newSingleThreadExecutor().execute(() -> {
-                AVUser.becomeWithSessionTokenInBackground(app_pref.getString("account_token", "")).subscribe(new Observer<AVUser>() {
-                    public void onSubscribe(Disposable disposable) {
-                    }
-
-                    public void onNext(AVUser user) {
-                        AVUser.changeCurrentUser(user, true);
-                        HAS_ACCOUNT = true;
-                        app_pref.edit().putString("custom_username", user.getString("custom_username")).apply();
-                        Logger.d(TAG, "Succeed.");
-
-                    }
-
-                    public void onError(Throwable throwable) {
-                        AVUser.changeCurrentUser(null, true);
-                        app_pref.edit().putBoolean("has_account", false)
-                                .putString("custom_username", "崩坏3扫码器用户").apply();
-                        throwable.printStackTrace();
-                        Logger.d(TAG, "Failed.");
-                        Log.makeToast("赞助者身份验证已过期...");
-                    }
-
-                    public void onComplete() {
-                    }
-                });
-            });
-
-        }
         switch (app_pref.getString("dark_type", "-1")) {
             case "-1":
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
@@ -158,67 +93,4 @@ public class MainApplication extends Application {
 
     }
 
-
-    @SuppressLint("HandlerLeak")
-    Handler update_check_hd = new Handler() {
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            super.handleMessage(msg);
-            Bundle data = msg.getData();
-            String feedback = data.getString("value");
-            feedback = feedback.substring(1, feedback.length() - 1).replaceAll("\\\\", "");
-            Logger.i("Update", "handleMessage: " + feedback);
-            try {
-                JSONObject json = new JSONObject(feedback);
-                app_pref.edit().putString("bh_ver", json.getString("bh_ver"))
-                        .putString("mdk_ver", json.getString("mdk_ver"))
-                        .putString("sp_url", json.getString("sp_url")).apply();
-
-                if (!getPackageName().contains("dev") && app_pref.getInt("version", VERSION_CODE) < json.getInt("ver")) {
-                    showUpdateDialog(
-                            json.getString("ver_name"),
-                            json.getString("update_url"),
-                            json.getString("logs").replaceAll("&n", "\n")
-                    );
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                Logger.d("Update", "Check Update Failed");
-
-                //app_pref.edit().putString("bh_ver", BH_VER).apply();
-            }
-            BH_VER = app_pref.getString("bh_ver", BH_VER);
-            MDK_VERSION = app_pref.getString("mdk_ver", MDK_VERSION);
-            SP_URL = app_pref.getString("sp_url", SP_URL);
-            AVOSCloud.initialize(getApplicationContext(), "VMh6lRyykuNDyhXxoi996cGI-gzGzoHsz", "RWvHCY9qXzX1BH4L72J9RI1I", SP_URL);
-            if (DEBUG) {
-                AVOSCloud.setLogLevel(AVLogger.Level.DEBUG);
-            }
-
-            Executors.newSingleThreadExecutor().execute(() -> new SponsorRepo(getApplicationContext()).refreshSponsors());
-
-        }
-    };
-
-
-    private void showUpdateDialog(String ver, String url, String logs) {
-        final AlertDialog.Builder normalDialog = new AlertDialog.Builder(this);
-        normalDialog.setTitle("获取到新版本: " + ver);
-        normalDialog.setMessage("更新日志：\n" + logs);
-        normalDialog.setPositiveButton("打开更新链接",
-                (dialog, which) -> {
-                    openUrl(url, this);
-                    dialog.dismiss();
-                });
-        normalDialog.setCancelable(false);
-        normalDialog.show();
-    }
-    Runnable update_rb = () -> {
-        String feedback = Network.sendPost("https://service-beurmroh-1256541670.sh.apigw.tencentcs.com/version", "");
-        Message msg = new Message();
-        Bundle data = new Bundle();
-        data.putString("value", feedback);
-        msg.setData(data);
-        update_check_hd.sendMessage(msg);
-    };
 }
