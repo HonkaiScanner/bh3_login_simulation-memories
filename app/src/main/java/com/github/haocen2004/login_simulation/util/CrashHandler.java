bb9ce972a5fd0e32067d2bab361960eaad5a1d86
Hao_cen <haocen.minecraft@qq.com>
2022-03-13 14:01:59 +0800

    修正错误日志保存路径
    Signed-off-by: Hao_cen <haocen.minecraft@qq.com>

--- app/src/main/java/com/github/haocen2004/login_simulation/util/CrashHandler.java
+++ app/src/main/java/com/github/haocen2004/login_simulation/util/CrashHandler.java
@@ -1,17 +1,15 @@
 package com.github.haocen2004.login_simulation.util;
 
-import static android.os.Environment.DIRECTORY_DOCUMENTS;
-
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.os.Environment;
 import android.os.Process;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.github.haocen2004.login_simulation.data.LogData;
 import com.github.haocen2004.login_simulation.data.LogLiveData;
 import com.tencent.bugly.crashreport.CrashReport;
 
@@ -22,21 +20,27 @@ import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Objects;
 
 public class CrashHandler implements Thread.UncaughtExceptionHandler {
     private static final String TAG = "CrashHandler";
     private static final CrashHandler instance = new CrashHandler();
     private Thread.UncaughtExceptionHandler mDefaultCrashHandler;
-    private final String PATH = Environment.getExternalStoragePublicDirectory(DIRECTORY_DOCUMENTS).getPath() + "/bh3_login_simulation/crash-report/";
+    private String PATH = "";
     private static final String FILE_NAME = "crash";
     private static final String FILE_NAME_SUFFIX = ".txt";
     private static Context mContext;
 
     @Override
     public void uncaughtException(@NonNull Thread t, @NonNull Throwable e) {
+        Log.e(TAG, "DETECT CRASH");
         try {
-            CrashReport.postCatchedException(e, t);
+            Log.d(TAG, "DUMP TO SDCARD");
+//            Log.d(TAG,"PATH:" + PATH);
             dumpExceptionToSDCard(e);
+            Log.d(TAG, "DUMP TO SDCARD SUCCEED");
+            CrashReport.postCatchedException(e, t);
+
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -64,6 +68,7 @@ public class CrashHandler implements Thread.UncaughtExceptionHandler {
         mDefaultCrashHandler = Thread.getDefaultUncaughtExceptionHandler();
         Thread.setDefaultUncaughtExceptionHandler(this);
         mContext = context;
+        PATH = mContext.getExternalFilesDir(null) + "/crash-report/";
     }
 
     private void dumpExceptionToSDCard(Throwable ex) {
@@ -74,9 +79,11 @@ public class CrashHandler implements Thread.UncaughtExceptionHandler {
         }
         long current = System.currentTimeMillis();
         String time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.CHINA).format(new Date(current));
-        File file = new File(PATH + FILE_NAME + time + FILE_NAME_SUFFIX);
+        time = time.replace(":", "-");
+        File file = new File(PATH + FILE_NAME + "-" + time + FILE_NAME_SUFFIX);
 
         try {
+            file.createNewFile();
             PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file)));
             pw.println(time);
             dumpPhoneInfo(pw);
@@ -84,10 +91,22 @@ public class CrashHandler implements Thread.UncaughtExceptionHandler {
             ex.printStackTrace(pw);
             pw.println();
             pw.println("Logs: ");
-            pw.println(LogLiveData.getINSTANCE(mContext).getValue());
+            dumpLogs(pw);
             pw.close();
         } catch (Exception e) {
             Log.e(TAG, "dump crash info failed");
+            e.printStackTrace();
+        }
+    }
+
+    private void dumpLogs(PrintWriter pw) {
+        for (LogData logData : Objects.requireNonNull(LogLiveData.getINSTANCE(mContext).getValue())) {
+            if (logData.getTAG().equals("复制日志")) continue;
+            pw.print(logData.getLevel());
+            pw.print("/");
+            pw.print(logData.getTAG());
+            pw.print(": ");
+            pw.println(logData.getMessage());
         }
     }
 
@@ -96,14 +115,16 @@ public class CrashHandler implements Thread.UncaughtExceptionHandler {
         PackageInfo pi = pm.getPackageInfo(mContext.getPackageName(), PackageManager.GET_ACTIVITIES);
         pw.print("App Version: ");
         pw.print(pi.versionName);
-        pw.print('_');
-        pw.println(pi.versionCode);
+        pw.print(" (");
+        pw.print(pi.versionCode);
+        pw.println(")");
 
         //android版本号
         pw.print("OS Version: ");
         pw.print(Build.VERSION.RELEASE);
-        pw.print("_");
-        pw.println(Build.VERSION.SDK_INT);
+        pw.print("(");
+        pw.print(Build.VERSION.SDK_INT);
+        pw.println(")");
 
         //手机制造商
         pw.print("Vendor: ");
@@ -114,7 +135,7 @@ public class CrashHandler implements Thread.UncaughtExceptionHandler {
         pw.println(Build.MODEL);
 
         //cpu架构
-        pw.print("CPU SUPPORTED ABI: ");
+        pw.print("SUPPORTED ABI: ");
         StringBuilder supportedABI = new StringBuilder();
         for (String abi : Build.SUPPORTED_ABIS) {
             supportedABI.append(abi);
