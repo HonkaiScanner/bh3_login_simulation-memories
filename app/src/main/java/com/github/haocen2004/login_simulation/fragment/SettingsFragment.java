3eb260e7a005c0083cb48dd7f6283f6c8224643b
Hao_cen <haocen.minecraft@qq.com>
2022-04-20 21:59:30 +0800

    修改自定义版本号为独立复写操作 修复独立设置界面的应用宝服显示问题
    Signed-off-by: Hao_cen <haocen.minecraft@qq.com>

--- app/src/main/java/com/github/haocen2004/login_simulation/fragment/SettingsFragment.java
+++ app/src/main/java/com/github/haocen2004/login_simulation/fragment/SettingsFragment.java
@@ -51,10 +51,23 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             return true;
         });
 
-        findPreference("bh_ver").setOnPreferenceChangeListener((preference, newValue) -> {
+        findPreference("bh_ver_overwrite").setOnPreferenceChangeListener((preference, newValue) -> {
+            if ((Boolean) newValue) {
+                BH_VER = app_pref.getString("custom_bh_ver", BH_VER);
+            } else {
+                BH_VER = app_pref.getString("bh_ver", BH_VER);
+            }
+            Logger.d(TAG, "new bh version:" + BH_VER);
+            return true;
+        });
+
+        findPreference("custom_bh_ver").setOnPreferenceChangeListener((preference, newValue) -> {
             Logger.d(TAG, newValue.toString());
-            app_pref.edit().putString("bh_ver", newValue.toString());
-            BH_VER = newValue.toString();
+            app_pref.edit().putString("custom_bh_ver", newValue.toString());
+            if (app_pref.getBoolean("bh_ver_overwrite", false)) {
+                BH_VER = newValue.toString();
+            }
+            Logger.d(TAG, "new bh version:" + BH_VER);
             return true;
         });
 
@@ -95,6 +108,9 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 case "Flyme":
                     preference.setSummary(getString(R.string.types_flyme));
                     break;
+                case "YYB":
+                    preference.setSummary(getString(R.string.types_yyb));
+                    break;
                 default:
                     preference.setSummary("DEBUG -- SERVER ERROR");
             }
@@ -122,6 +138,9 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 break;
             case "Flyme":
                 findPreference("server_type").setSummary(getString(R.string.types_flyme));
+                break;
+            case "YYB":
+                findPreference("server_type").setSummary(getString(R.string.types_yyb));
                 break;
             default:
                 findPreference("server_type").setSummary("DEBUG -- SERVER ERROR");
