f2162034494be7216ba9f6c7d1ed774eb3ef8e7c
Hao_cen <haocen.minecraft@qq.com>
2023-04-07 22:13:26 +0800

    fix pure dark wrong display on light mode
    Signed-off-by: Hao_cen <haocen.minecraft@qq.com>

--- app/src/main/java/com/github/haocen2004/login_simulation/utils/ThemeUtils.java
+++ app/src/main/java/com/github/haocen2004/login_simulation/utils/ThemeUtils.java
@@ -2,6 +2,7 @@ package com.github.haocen2004.login_simulation.utils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 
 import androidx.preference.PreferenceManager;
 
@@ -12,12 +13,16 @@ public class ThemeUtils {
 
     public static int getNightThemeStyleRes(Context context) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-        if (sharedPreferences.getBoolean("pure_black_theme", false)) {
+        if (sharedPreferences.getBoolean("pure_black_theme", false) && isNightMode(context.getResources().getConfiguration())) {
             return R.style.ThemeOverlay_Black;
         } else {
             return R.style.ThemeOverlay;
         }
 
+    }
+
+    public static boolean isNightMode(Configuration configuration) {
+        return (configuration.uiMode & Configuration.UI_MODE_NIGHT_YES) > 0;
     }
 
 }
