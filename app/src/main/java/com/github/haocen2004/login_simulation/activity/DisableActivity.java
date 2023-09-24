e941996da8a9e2b1def94a3e847c94567f47798b
Hao_cen <haocen.minecraft@qq.com>
2023-09-24 17:44:34 +0800

    add disable auto exit
    Signed-off-by: Hao_cen <haocen.minecraft@qq.com>

--- app/src/main/java/com/github/haocen2004/login_simulation/activity/DisableActivity.java
+++ app/src/main/java/com/github/haocen2004/login_simulation/activity/DisableActivity.java
@@ -1,16 +1,28 @@
 package com.github.haocen2004.login_simulation.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
+import androidx.annotation.NonNull;
 
 import com.github.haocen2004.login_simulation.databinding.ActivityDisableBinding;
 
 public class DisableActivity extends BaseActivity {
+    private final Handler disableDelay = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityDisableBinding binding = ActivityDisableBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        disableDelay.postDelayed(() -> ActivityManager.getInstance().clearActivity(), 30000);
     }
 
     @Override
